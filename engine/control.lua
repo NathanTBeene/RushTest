42b6f59675e3e22303078d4e90255d1a80796c54
Nathan Beene <beene.nathan@gmail.com>
2026-01-06 16:58:04 -0600

    Fix documentation.

--- engine/control.lua
+++ engine/control.lua
@@ -9,6 +9,8 @@ function Control:init()
   Control.super.init(self)
 end
 
+--- Checks if the mouse is currently over this control
+--- @return boolean True if the mouse is over the control, false otherwise
 function Control:is_mouse_over()
   local mousePos = Vector2(love.mouse.getX(), love.mouse.getY())
   return self:is_in_bounds(mousePos)
