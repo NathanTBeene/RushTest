839868a171f0028b7fcb1172d6418c4430af9a0b
Nathan Beene <beene.nathan@gmail.com>
2026-01-20 16:37:19 -0600

    Update Control to remove deprecated rect handling since it's handled by Node now.

--- engine/control.lua
+++ engine/control.lua
@@ -5,35 +5,9 @@ Control = Node:extend("Control")
 ---* interacted with separately from the rest of the game world. They are on their
 ---* own layer, and are not affected by the game world's physics or other systems.
 
-
-local DraggableMixin = require("engine.mixins.draggablemixin")
-Control:implement(DraggableMixin)
-
 function Control:init()
   Control.super.init(self)
 
-  -- Controls are represented by Rects
-  self.rect = Rect(0, 0, 100, 100)
-
-  -- Aliasing
-  self:define_property("size",
-    function(self) return Vector2(self.rect.size.x, self.rect.size.y) end,
-    function(self, value)
-      self.rect.size.x = value.x
-      self.rect.size.y = value.y
-    end
-  )
-  self:define_property("width",
-    function(self) return self.rect.size.x end,
-    function(self, value) self.rect.size.x = value end
-  )
-  self:define_property("height",
-    function(self) return self.rect.size.y end,
-    function(self, value) self.rect.size.y = value end
-  )
-
-  -- Draggable
-  self:init_draggable()
 end
 
 -- ---------------------------- LIFECYCLE METHODS --------------------------- --
@@ -42,26 +16,9 @@ function Control:_load()
 end
 
 function Control:_update(dt)
-  self:update_drag()
 end
 
 function Control:_draw()
-  self:draw_bounds(Color.purple)
-end
-
--- --------------------------- BOUNDARY CHECKERS ---------------------------- --
-
---- Creates a rectangle representing the bounds of the control in global space
---- @return Rect The bounding rectangle of the control
-function Control:get_bounds()
-  return self.rect:get_bounds()
-end
-
---- Checks if a given point is within the bounds of the control
---- @param point Vector2 The point to check
---- @return boolean True if the point is within bounds, false otherwise
-function Control:is_in_bounds(point)
-  return self.rect:has_point(point)
 end
 
 -- -------------------------------- CHECKERS -------------------------------- --
@@ -71,19 +28,4 @@ end
 function Control:is_mouse_over()
   local mousePos = I:get_mouse_position()
   return self:is_in_bounds(mousePos)
-end
-
--- ---------------------------------- DEBUG --------------------------------- --
-
---- Draws the bounding rectangle of the control for debugging purposes
-function Control:draw_bounds(color, thickness)
-  if not self.debug then return end
-
-  local choice = color or Color.red
-  local thick = thickness or 2
-
-  love.graphics.setColor(choice.r, choice.g, choice.b, choice.a)
-  love.graphics.setLineWidth(thick)
-  love.graphics.rectangle("line", self.global_position.x, self.global_position.y, self.size.x, self.size.y)
-  love.graphics.setColor(1, 1, 1, 1)
 end
